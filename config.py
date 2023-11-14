b04d8bc01bb1a875634dd40a05126d3d002baf13
Typheta <73000873+Typheta@users.noreply.github.com>
2023-11-14 02:46:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -91,6 +91,68 @@ def returnByStudentID(student_id):
     cursor.close()
     return result
 
+def update_student(student_id, new_values):
+   
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE Student SET FirstName=%s, LastName=%s, DOB=%s, Email=%s, Major=%s WHERE StudentID=%s", (new_values['first_name'], new_values['last_name'], new_values['dob'], new_values['email'], new_values['major'], student_id))
+        mydb.commit()
+        return f"Success: Student with ID {student_id} updated."
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return f"Error updating student."
+
+def update_professor(professor_id, new_values):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE Professor SET FirstName=%s, LastName=%s, DOB=%s, Department=%s, Email=%s WHERE ProfessorID=%s", (new_values['first_name'], new_values['last_name'], new_values['dob'], new_values['department'], new_values['email'], professor_id))
+        mydb.commit()
+        return f"Success: Professor with ID {professor_id} updated."
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return f"Error updating professor."
+
+def update_classroom(classroom_id, new_values):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE Classroom SET Building=%s, RoomNumber=%s, Capacity=%s WHERE ClassroomID=%s", (new_values['building'], new_values['room_number'], new_values['capacity'], classroom_id))
+        mydb.commit()
+        return f"Success: Classroom with ID {classroom_id} updated."
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return f"Error updating classroom."
+
+def update_course(course_id, new_values):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE Course SET CourseName=%s, ClassroomID=%s, CourseYear=%s, CourseDescription=%s, ProfessorID=%s WHERE CourseID=%s", (new_values['course_name'], new_values['classroom_id'], new_values['course_year'], new_values['course_description'], new_values['professor_id'], course_id))
+        mydb.commit()
+        return f"Success: Course with ID {course_id} updated."
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return f"Error updating course."
+
+def update_grade(grade_id, new_values):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE Grades SET StudentID=%s, CourseID=%s, Semester=%s, LetterGrade=%s, NumericGrade=%s WHERE GradeID=%s", (new_values['student_id'], new_values['course_id'], new_values['semester'], new_values['letter_grade'], new_values['numeric_grade'], grade_id))
+        mydb.commit()
+        return f"Success: Grade with ID {grade_id} updated."
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return f"Error updating grade."
+
+def update_enrollment(enrollment_id, new_values):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("UPDATE Enrollment SET StudentID=%s, CourseID=%s, EnrollmentDate=%s WHERE EnrollmentID=%s", (new_values['student_id'], new_values['course_id'], new_values['enrollment_date'], enrollment_id))
+        mydb.commit()
+        return f"Success: Enrollment with ID {enrollment_id} updated."
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return f"Error updating enrollment."
+
+
 # Function to find students with the highest GPA
 def returnGPAbyStudentID(student_id):
     try:
