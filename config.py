a147a061670ef901f437c2ca55c05d61aadb1669
nolanallensmith <122489024+nolanallensmith@users.noreply.github.com>
2023-11-14 00:07:37 -0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,94 @@
+# config.py
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="godawgs32",
+    database="project"
+)
+print(mydb)
+
+def insert_student(student_id, first_name, last_name, dob, email, major):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO Student VALUES (%s, %s, %s, %s, %s, %s)",
+                       (student_id, first_name, last_name, dob, email, major))
+        mydb.commit()
+        return "Student inserted successfully!"
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "Error inserting student."
+
+def insert_professor(professor_id, first_name, last_name, dob, department, email):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO Professor VALUES (%s, %s, %s, %s, %s, %s)",
+                       (professor_id, first_name, last_name, dob, department, email))
+        mydb.commit()
+        return "Professor inserted successfully!"
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "Error inserting professor."
+
+def insert_classroom(classroom_id, building, room_number, capacity):
+    try:
+        cursor = mydb.cursor()
+        cursor.execute("INSERT INTO Classroom VALUES (%s, %s, %s, %s)",
+                       (classroom_id, building, room_number, capacity))
+        mydb.commit()
+        return "Classroom inserted successfully!"
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "Error inserting classroom."
+    finally:
+        cursor.close()
+
+def insert_course(course_id, course_name, classroom_id, course_year, course_description, professor_id):
+    try:
+        # Assuming you have a table named Course with appropriate columns
+        mycursor.execute("INSERT INTO Course VALUES (%s, %s, %s, %s, %s, %s)",
+                       (course_id, course_name, classroom_id, course_year, course_description, professor_id))
+        mydb.commit()
+        return "Course inserted successfully!"
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "Error inserting course."
+    finally:
+        mycursor.close()
+
+def insert_grade(grade_id, student_id, course_id, semester, letter_grade, numeric_grade):
+    try:
+        # Assuming you have a table named Grades with appropriate columns
+        mycursor.execute("INSERT INTO Grades VALUES (%s, %s, %s, %s, %s, %s)",
+                       (grade_id, student_id, course_id, semester, letter_grade, numeric_grade))
+        mydb.commit()
+        return "Grade inserted successfully!"
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "Error inserting grade."
+    finally:
+        mycursor.close()
+
+def insert_enrollment(enrollment_id, student_id, course_id, enrollment_date):
+    try:
+        # Assuming you have a table named Enrollment with appropriate columns
+        mycursor.execute("INSERT INTO Enrollment VALUES (%s, %s, %s, %s)",
+                       (enrollment_id, student_id, course_id, enrollment_date))
+        mydb.commit()
+        return "Enrollment inserted successfully!"
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        return "Error inserting enrollment."
+    finally:
+        mycursor.close()
+
+def returnByStudentID(StudentID):
+    cursor = mydb.cursor()
+    cursor.execute("SELECT * FROM Student WHERE StudentID = %s;", (StudentID,))
+    result = cursor.fetchone()
+    cursor.close()
+    return result
+
+
---
+++ routes.py
@@ -0,0 +1,138 @@
+# routes.py
+
+from flask import render_template, request
+from app import app
+from config import insert_student, insert_professor, insert_classroom, insert_course, insert_grade, insert_enrollment
+from config import returnByStudentID
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home')
+
+# Route to handle the form submission for inserting a student
+@app.route('/insert_student', methods=['POST'])
+def insert_student_route():
+    if request.method == 'POST':
+        student_id = request.form['student_id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        dob = request.form['dob']
+        email = request.form['email']
+        major = request.form['major']
+
+        result = insert_student(student_id, first_name, last_name, dob, email, major)
+        
+        # Check if the insertion was successful
+        if "successfully" in result.lower():
+            return f"{result}<br><a href='/'>Return to Home</a>"
+        else:
+            return f"{result}<br><a href='/'>Back to Home</a>"
+    else:
+        return "Invalid request method"
+
+
+# Route to handle the form submission for inserting a professor
+@app.route('/insert_professor', methods=['POST'])
+def insert_professor_route():
+    if request.method == 'POST':
+        professor_id = request.form['professor_id']
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        dob = request.form['dob']
+        department = request.form['department']
+        email = request.form['email']
+
+        result = insert_professor(professor_id, first_name, last_name, dob, department, email)
+        
+        # Check if the insertion was successful
+        if "successfully" in result.lower():
+            return f"{result}<br><a href='/'>Return to Home</a>"
+        else:
+            return f"{result}<br><a href='/'>Back to Home</a>"
+    else:
+        return "Invalid request method"
+
+# Route to handle the form submission for inserting a classroom
+@app.route('/insert_classroom', methods=['POST'])
+def insert_classroom_route():
+    if request.method == 'POST':
+        classroom_id = request.form['classroom_id']
+        building = request.form['building']
+        room_number = request.form['room_number']
+        capacity = request.form['capacity']
+
+        result = insert_classroom(classroom_id, building, room_number, capacity)
+       
+       # Check if the insertion was successful
+        if "successfully" in result.lower():
+            return f"{result}<br><a href='/'>Return to Home</a>"
+        else:
+            return f"{result}<br><a href='/'>Back to Home</a>"
+    else:
+        return "Invalid request method"
+
+@app.route('/insert_course', methods=['POST'])
+def insert_course_route():
+    if request.method == 'POST':
+        course_id = request.form['course_id']
+        course_name = request.form['course_name']
+        classroom_id = request.form['classroom_id']
+        course_year = request.form['course_year']
+        course_description = request.form['course_description']
+        professor_id = request.form['professor_id']
+
+        result = insert_course(course_id, course_name, classroom_id, course_year, course_description, professor_id)
+        
+        # Check if the insertion was successful
+        if "successfully" in result.lower():
+            return f"{result}<br><a href='/'>Return to Home</a>"
+        else:
+            return f"{result}<br><a href='/'>Back to Home</a>"
+    else:
+        return "Invalid request method"
+
+@app.route('/insert_grade', methods=['POST'])
+def insert_grade_route():
+    if request.method == 'POST':
+        grade_id = request.form['grade_id']
+        student_id = request.form['student_id']
+        course_id = request.form['course_id']
+        semester = request.form['semester']
+        letter_grade = request.form['letter_grade']
+        numeric_grade = request.form['numeric_grade']
+
+        result = insert_grade(grade_id, student_id, course_id, semester, letter_grade, numeric_grade)
+        
+        # Check if the insertion was successful
+        if "successfully" in result.lower():
+            return f"{result}<br><a href='/'>Return to Home</a>"
+        else:
+            return f"{result}<br><a href='/'>Back to Home</a>"
+    else:
+        return "Invalid request method"
+
+@app.route('/insert_enrollment', methods=['POST'])
+def insert_enrollment_route():
+    if request.method == 'POST':
+        enrollment_id = request.form['enrollment_id']
+        student_id = request.form['student_id']
+        course_id = request.form['course_id']
+        enrollment_date = request.form['enrollment_date']
+
+        result = insert_enrollment(enrollment_id, student_id, course_id, enrollment_date)
+        # Check if the insertion was successful
+        if "successfully" in result.lower():
+            return f"{result}<br><a href='/'>Return to Home</a>"
+        else:
+            return f"{result}<br><a href='/'>Back to Home</a>"
+    else:
+        return "Invalid request method"
+
+@app.route('/search', methods=['POST'])
+def search():
+    # Retrieve the student_id from the form
+    student_id = request.form['student_id']
+
+    # Call the function to get student details
+    student_details = returnByStudentID(int(student_id))  # Convert to int
+
+    return render_template('search_result.html', title='Search Result', student_details=student_details)
